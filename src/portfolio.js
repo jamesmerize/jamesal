d3faa2d667e0b8bf78ed2941b230cc62797cb8fe
unknown <engg.james@gmail.com>
2025-04-07 22:23:32 +0530

    first commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,10 +20,10 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "James M.",
+  title: "Hi all, I'm James",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "Passionate Full Stack Software Developer 🚀 having an experience of building AI ChatBots and  Web applications with Python/PHP/JavaScript/HTML/CSS  using Flask/FastAPI/Nodejs and some other cool libraries and frameworks."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
